ed36ab508d7b7244be323fbcf832f9711ea713c7
Brooke <hi@broo.ke>
2019-12-22 02:05:34 -0800

    Update for PHPCS standards

--- ackee-wp.php
+++ ackee-wp.php
@@ -20,80 +20,69 @@
  * Domain Path:       /languages
  * License:           GPL v3 or later
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
- 
+
  * Ackee WP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- * 
+ *
  * Ackee WP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  * along with Ackee WP. If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
  */
 
-
-if (! defined('ABSPATH') ) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
  * Main AckeeWP class.
  */
-class AckeeWP
-{
+class AckeeWP {
 
-    /**
-     * Constructor.
-     */
-    public function __construct()
-    {
-        // Ackee WP constants.
-        define('ACKEE_WP_FILE', __FILE__);
-        define('ACKEE_WP_DIR', trailingslashit(dirname(__FILE__)));
-        define('ACKEE_WP_VERSION', '1.0.0');
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// Ackee WP constants.
+		define( 'ACKEE_WP_FILE', __FILE__ );
+		define( 'ACKEE_WP_DIR', trailingslashit( dirname( __FILE__ ) ) );
+		define( 'ACKEE_WP_VERSION', '1.0.0' );
 
-        register_activation_hook(basename(ACKEE_WP_DIR) . '/' . basename(ACKEE_WP_FILE), array( $this, 'activate' ));
+		register_activation_hook( basename( ACKEE_WP_DIR ) . '/' . basename( ACKEE_WP_FILE ), array( $this, 'activate' ) );
 
-        add_action('init', array( $this, 'load_plugin_textdomain' ));
-        add_action('plugins_loaded', array( $this, 'includes' ));
-        register_uninstall_hook(ACKEE_WP_FILE, 'uninstall');
-    }
-    /**
-     * Textdomain.
-     */
-    public function load_plugin_textdomain()
-    {
-        load_plugin_textdomain('ackeewp', false, dirname(plugin_basename(ACKEE_WP_FILE)) . '/languages/');
-    }
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+		add_action( 'plugins_loaded', array( $this, 'includes' ) );
+		register_uninstall_hook( ACKEE_WP_FILE, 'uninstall' );
+	}
+	/**
+	 * Textdomain.
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( 'ackeewp', false, dirname( plugin_basename( ACKEE_WP_FILE ) ) . '/languages/' );
+	}
 
-    /**
-     * Includes.
-     */
-    public function includes()
-    {
-                include_once ACKEE_WP_DIR . 'includes/class-admin-settings.php';
-                include_once ACKEE_WP_DIR . 'includes/class-frontend.php';
-    }
+	/**
+	 * Includes.
+	 */
+	public function includes() {
+		include_once ACKEE_WP_DIR . 'includes/class-admin-settings.php';
+		include_once ACKEE_WP_DIR . 'includes/class-frontend.php';
+	}
 
-     /**
-      * Remove option at uninstall
-      */
-    private static function uninstall()
-    {
-
-
-        if (! defined('WP_UNINSTALL_PLUGIN') ) {
-            die;
-        }
-        
-        $option_name = 'ackeewp';
-        
-        delete_option($option_name);
-    }
+	/**
+	 * Remove option at uninstall
+	 */
+	private static function uninstall() {
+		if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+			die;
+		}
+		$option_name = 'ackeewp';
+		delete_option( $option_name );
+	}
 }
-
 new AckeeWP();
--- includes/class-frontend.php
+++ includes/class-frontend.php
@@ -6,7 +6,7 @@
  *
  * @package   Ackee_WP
  * @author    Brooke.
- * @copyright 2020 Brooke.
+ * @copyright 2019 Brooke.
  * @license   GPL-3.0-or-later
  * @link      https://brooke.codes/plugins/ackee-wp
  */
@@ -16,71 +16,69 @@ namespace AckeeWP;
 /**
  * Adds the WordPress settings page
  */
-if (! class_exists('AckeeWP_Frontend') ) :
-    class AckeeWP_Frontend
-    {
+if ( ! class_exists( 'AckeeWP_Frontend' ) ) :
+	class AckeeWP_Frontend {
 
-        /**
-         * Constructor.
-         */
-        public function __construct()
-        {
-            add_action('wp_enqueue_scripts', array($this, 'ackeewp_maybe_show_script' ));
-        }
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+			add_action( 'wp_enqueue_scripts', array( $this, 'ackeewp_maybe_show_script' ) );
+		}
 
-        /**
-         * Hooks into wp_enqueue_script to add the script to the footer.
-         *
-         * @since  1.0.0
-         * @access public
-         */
+		/**
+		 * Hooks into wp_enqueue_script to add the script to the footer.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 */
+		public function ackeewp_maybe_show_script() {
+			// NOTE TO SELF If NO SETTING.
 
-        public function ackeewp_maybe_show_script()
-        {
-            //return if we are ignoring logged in visits and the user is logged in
-            if(true === (bool)$this->ackeewp_get_options('exclude_logged_in') && is_user_logged_in() ) {
-                return;
-            }
-            //otherwise  register and enqueue the script
-            add_filter('script_loader_tag', array( $this, 'ackeewp_generate_script'), 10, 3);
-            $ackee_tracking_url = trailingslashit($this->ackeewp_get_options('instance_url')) . $this->ackeewp_get_options('tracking_script');
-            
-            wp_register_script('ackeewp', $ackee_tracking_url, '', ACKEE_WP_VERSION, 'true');
-            wp_enqueue_script('ackeewp');
-        }
+			// return if we are ignoring logged in visits and the user is logged in.
+			if ( true === (bool) $this->ackeewp_get_options( 'exclude_logged_in' ) && is_user_logged_in() ) {
+				return;
+			}
+			// otherwise  register and enqueue the script.
+			add_filter( 'script_loader_tag', array( $this, 'ackeewp_generate_script' ), 10, 3 );
 
-        /**
-         * Renders the Ackee script based on the options
-         *
-         * @since  1.0.0
-         * @access private
-         */
-        public function ackeewp_generate_script( $tag, $handle, $src )
-        {
-            if ('ackeewp' === $handle ) {
-                 $tag = '<script async src="' . $src . '" data-ackee-server="' . $this->ackeewp_get_options('instance_url')  . '" data-ackee-domain-id="' . $this->ackeewp_get_options('domain_id') .'"></script>';
-            }
-            return $tag;
-        }
-        /**
-         * Renders the Ackee script based on the options
-         *
-         * @since  1.0.0
-         * @access private
-         */
-        private function ackeewp_get_options( $option_key )
-        {
-            $ackee_settings = get_option('ackeewp_settings');
-            //reutrn if option key is not in the settings array
-            if(! is_array($ackee_settings) || ! array_key_exists($option_key, $ackee_settings) ) {
-                return;
-            }
-            return ( 'instance_url' ===  $option_key ) ? esc_url_raw($ackee_settings[ $option_key ]) : sanitize_text_field($ackee_settings[ $option_key ]);
-        }
-      
-    } //end of class
+			$ackee_tracking_url = trailingslashit( $this->ackeewp_get_options( 'instance_url' ) ) . $this->ackeewp_get_options( 'tracking_script' );
 
-    new AckeeWP_Frontend();
+			wp_register_script( 'ackeewp', $ackee_tracking_url, '', ACKEE_WP_VERSION, 'true' );
+			wp_enqueue_script( 'ackeewp' );
+		}
 
-endif; 
+		/**
+		 * Renders the Ackee script based on the options
+		 *
+		 * @since  1.0.0
+		 * @access private
+		 */
+		public function ackeewp_generate_script( $tag, $handle, $src ) {
+			if ( 'ackeewp' === $handle ) {
+				$tag = '<script async src="' . $src . '" data-ackee-server="' . $this->ackeewp_get_options( 'instance_url' ) . '"data-ackee-domain-id="' . $this->ackeewp_get_options( 'domain_id' ) . '"></script>'; // phpcs:ignore
+			}
+			return $tag;
+		}
+
+		/**
+		 * Renders the Ackee script based on the options
+		 *
+		 * @since  1.0.0
+		 * @access private
+		 *
+		 * $option_key
+		 */
+		private function ackeewp_get_options( $option_key ) {
+			$ackee_settings = get_option( 'ackeewp_settings' );
+			// reutrn if option key is not in the settings array.
+			if ( ! is_array( $ackee_settings ) || ! array_key_exists( $option_key, $ackee_settings ) ) {
+				return;
+			}
+
+			return ( 'instance_url' === $option_key ) ? esc_url_raw( $ackee_settings[ $option_key ] ) : sanitize_text_field( $ackee_settings[ $option_key ] );
+		}
+	} //end of class
+	new AckeeWP_Frontend();
+endif;
 
