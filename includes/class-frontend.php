fd6e4871a0a498db3456975670b45053a18dfec5
Brooke <bandonrandon@gmail.com>
2019-12-29 23:16:40 +0000

    Release 1.0

--- includes/class-frontend.php
+++ includes/class-frontend.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:ignore -- ignore class naming
 /**
  * Frontend Output
  *
@@ -6,81 +6,99 @@
  *
  * @package   Ackee_WP
  * @author    Brooke.
- * @copyright 2020 Brooke.
+ * @copyright 2019 Brooke.
  * @license   GPL-3.0-or-later
- * @link      https://brooke.codes/plugins/ackee-wp
+ * @link      https://brooke.codes/projects/ackee-wp
  */
 
 namespace AckeeWP;
 
-/**
- * Adds the WordPress settings page
- */
-if (! class_exists('AckeeWP_Frontend') ) :
-    class AckeeWP_Frontend
-    {
+if ( ! class_exists( 'AckeeWP_Frontend' ) ) :
+	/**
+	 * Class to Handle the front end display of the JavaScript output.
+	 */
+	class AckeeWP_Frontend {
 
-        /**
-         * Constructor.
-         */
-        public function __construct()
-        {
-            add_action('wp_enqueue_scripts', array($this, 'ackeewp_maybe_show_script' ));
-        }
+		/**
+		 * Constructor
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 */
+		public function __construct() {
+			add_action( 'wp_enqueue_scripts', array( $this, 'ackeewp_maybe_show_script' ) );
+		}
 
-        /**
-         * Hooks into wp_enqueue_script to add the script to the footer.
-         *
-         * @since  1.0.0
-         * @access public
-         */
+		/**
+		 * Hooks into wp_enqueue_script to add the script to the footer.
+		 *
+		 * @since  1.0.0
+		 * @access public
+		 */
+		public function ackeewp_maybe_show_script() {
+			/* Bail if the options haven't been saved yet */
+			if ( ! get_option( 'ackeewp_settings' ) ) {
+				return;
+			}
 
-        public function ackeewp_maybe_show_script()
-        {
-            //return if we are ignoring logged in visits and the user is logged in
-            if(true === (bool)$this->ackeewp_get_options('exclude_logged_in') && is_user_logged_in() ) {
-                return;
-            }
-            //otherwise  register and enqueue the script
-            add_filter('script_loader_tag', array( $this, 'ackeewp_generate_script'), 10, 3);
-            $ackee_tracking_url = trailingslashit($this->ackeewp_get_options('instance_url')) . $this->ackeewp_get_options('tracking_script');
-            
-            wp_register_script('ackeewp', $ackee_tracking_url, '', ACKEE_WP_VERSION, 'true');
-            wp_enqueue_script('ackeewp');
-        }
+			/* Don't display the script if we are not tracking logged in visits and the user is logged in. */
+			if ( true === (bool) $this->ackeewp_get_options( 'exclude_logged_in' ) && is_user_logged_in() ) {
+				return;
+			}
 
-        /**
-         * Renders the Ackee script based on the options
-         *
-         * @since  1.0.0
-         * @access private
-         */
-        public function ackeewp_generate_script( $tag, $handle, $src )
-        {
-            if ('ackeewp' === $handle ) {
-                 $tag = '<script async src="' . $src . '" data-ackee-server="' . $this->ackeewp_get_options('instance_url')  . '" data-ackee-domain-id="' . $this->ackeewp_get_options('domain_id') .'"></script>';
-            }
-            return $tag;
-        }
-        /**
-         * Renders the Ackee script based on the options
-         *
-         * @since  1.0.0
-         * @access private
-         */
-        private function ackeewp_get_options( $option_key )
-        {
-            $ackee_settings = get_option('ackeewp_settings');
-            //reutrn if option key is not in the settings array
-            if(! is_array($ackee_settings) || ! array_key_exists($option_key, $ackee_settings) ) {
-                return;
-            }
-            return ( 'instance_url' ===  $option_key ) ? esc_url_raw($ackee_settings[ $option_key ]) : sanitize_text_field($ackee_settings[ $option_key ]);
-        }
-      
-    } //end of class
+			/* All conditions are met to display the script to proceed with register and enqueue the script. */
+			add_filter( 'script_loader_tag', array( $this, 'ackeewp_generate_script' ), 10, 3 );
 
-    new AckeeWP_Frontend();
+			$ackee_tracking_url = trailingslashit( $this->ackeewp_get_options( 'instance_url' ) ) . $this->ackeewp_get_options( 'tracking_script' );
 
-endif; 
+			wp_register_script( 'ackeewp', $ackee_tracking_url, '', ACKEE_WP_VERSION, 'true' );
+			wp_enqueue_script( 'ackeewp' );
+		}
+
+		/**
+		 * Renders the Ackee script based on the options
+		 *
+		 * @since  1.0.0
+		 * @access private
+		 * @link https://developer.wordpress.org/reference/hooks/script_loader_tag/
+		 * @param string $tag    The `<script>` tag for the enqueued script.
+		 * @param string $handle The script's registered handle.
+		 * @param string $src    The script's source URL.
+		 * @return string
+		 */
+		public function ackeewp_generate_script( $tag, $handle, $src ) {
+			if ( 'ackeewp' === $handle ) {
+				$tag = '<script async src="' . $src . '" data-ackee-server="' . $this->ackeewp_get_options( 'instance_url' ) . '" data-ackee-domain-id="' . $this->ackeewp_get_options( 'domain_id' ) . '"></script>'; // phpcs:ignore
+			}
+			return $tag;
+		}
+
+		/**
+		 * Renders the Ackee script based on the options
+		 *
+		 * @since  1.0.0
+		 * @access private
+		 * @param string $option_key The key of the value we want from the ackeewp_settings option.
+		 * @return string
+		 */
+		private function ackeewp_get_options( $option_key ) {
+			$ackee_settings = get_option( 'ackeewp_settings' );
+
+			/* Bail if the supplied option key is not in the settings array or the setting is not an array. */
+			if ( ! is_array( $ackee_settings ) || ! array_key_exists( $option_key, $ackee_settings ) ) {
+				return;
+			}
+
+			/* Escape the URL if we are getting the instance_url otherwise escape the value */
+			if ( 'instance_url' === $option_key ) {
+				$option_value = esc_url_raw( $ackee_settings[ $option_key ] );
+			} else {
+				$option_value = sanitize_text_field( $ackee_settings[ $option_key ] );
+			}
+
+			return $option_value;
+		}
+	} //end of class
+	new AckeeWP_Frontend();
+endif;
 
